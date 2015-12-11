8a96102f17df1e75e9f6cacfec942ebd9d0f6a7b
arbiacharfy <arbia.charfi@yahoo.ca>
2015-12-10 22:05:45 -0500

    First commit

---
+++ JavaScript/canvas.js
@@ -0,0 +1,10 @@
+/**
+ * @author arbia
+ */
+
+var canvas = document.getElementById('canvasLogo');
+var context = canvas.getContext('ed');
+// widh and height of the canvas
+canvas.width = "100px";
+canvas.height = "80px";
+ 
