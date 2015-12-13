3388cebed8973f6c4f65619b8863b01253989ad2
arbiacharfy <arbia.charfi@yahoo.ca>
2015-12-13 15:21:45 -0500

    commit du 13/12/2015 a 15h30

---
+++ JavaScript/hp.js
@@ -0,0 +1,32 @@
+var bdProduit = null;
+$(document).ready(function() {
+	bdProduit = localStorage.getItem('bdProduitjson');
+	bdProduit = JSON.parse(bdProduit);
+	var descJsonObjects = bdProduit.Produits;
+
+	for (var i = 0; i < descJsonObjects.length; i++) {
+		var jsonObject = null;
+		jsonObject = descJsonObjects[i];
+		if (jsonObject.categorieProduit == "HP") {
+			var li = '<li>';
+			li = li + jsonObject.nomProduit + "</li>";
+			$("#ul_Liste_Des_Articles:first").append(li);
+
+			// generer les sections
+
+			var section = "<section id='sectionHP" + (i + 1) + "'>";
+
+			var header = "<header><h1>Fabriquant du " + jsonObject.nomProduit + "</h1><a href='#sectionHP1'>Retour a la premiere section</a></header>";
+
+			var figure = "<figure><figcaption>" + jsonObject.nomProduit + "</figcaption><img src='../../images/" + jsonObject.imageProduit + "' width='320' height='228'></figure>";
+			var article = "<article><p>" + jsonObject.descriptionProduit + "</p></article>";
+			var footer = "<footer><p>Prix : 500$ <a href='#sectionHP" + descJsonObjects.length + "'>Aller a la derniere section</a></p></footer>";
+
+			section = section + header + figure + article + footer + "</section>";
+			$("#sectionCategories:first").append(section);
+		}
+	}
+
+	$('#Liste_Des_Articles').jstree();
+
+});
