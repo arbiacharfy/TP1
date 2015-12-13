b3a4224972d7b9ab6d974d6e54422b7294219bd2
arbiacharfy <arbia.charfi@yahoo.ca>
2015-12-13 03:13:50 -0500

    commit 2015

--- JavaScript/connexion.js
+++ JavaScript/connexion.js
@@ -21,10 +21,10 @@ function testConnexion() {
 		$("#connexion").hide();
 	}
 
-	$("#deconnecter").click(function() {
-		seDeconnecter();
-
-	});
+	// $("#deconnecter").click(function() {
+		// seDeconnecter();
+// 
+	// });
 }
 
 function SeConnecter() {
@@ -46,68 +46,6 @@ function SeConnecter() {
 		$("#connexion").eq(0).show();
 	}
 }
-
-// ******************** Formulaire Ajout Employe *************************/
-//
-// function enregistrer_Employe() {
-// var nom = $('input[name="nom"]:first').val();
-// var prenom = $('input[name="prenom"]:first').val();
-// var email = $('input[name="email"]:first').val();
-// var fonction = $('input[name="fonction"]:first').val();
-// var file = $('input[name="telechargerEmp"]:first').val();
-//
-// if ((nom == "undefined") || (nom == null) || (email == "undefined") || (email == null) || (prenom == "undefined") || (prenom == null)) {
-// alert("Merci de remplir les champs manquants");
-// retourn(false);
-// //on ne change rien si
-// }
-// //methode 1
-// bd = localStorage.getItem('bdjson');
-// if ((bd == "undefined") || (bd == null)) {//ici on doit initialiser la bd
-// var bdjson = {
-// "descriptions" : []
-//
-// };
-//
-// localStorage.setItem('bdjson', JSON.stringify(bdjson));
-// bd = bdjson;
-//
-// } else {
-// bd = JSON.parse(bd);
-// //parsing de objet json
-// }
-// var descJsonObjects = bd.descriptions;
-// var jsonObject = {//creation de json
-// "nom" : nom,
-// "prenom" : prenom,
-// "email" : email,
-// "fonction" : fonction,
-// "file" : file
-// };
-// descJsonObjects.push(jsonObject);
-// //ajout dans le tableau des description
-// bd.descriptions = descJsonObjects;
-// localStorage.setItem('bdjson', JSON.stringify(bd));
-// return (true);
-// }
-
-// var descJsonObjects = bd.descriptions;
-// var hr = "<hr>";
-// $("#contenu:first").append(hr);
-//
-// for (var i = 0; i < descJsonObjects.length; i++) {
-// var jsonObject = null;
-// jsonObject = descJsonObjects[i];
-// var div = '<div class="ajoutjson">';
-// div = div + '<h3 class="nom">' + jsonObject.nom + '</h3>';
-// div = div + '<img src=' + jsonObject.image + '>';
-// div = div + '<div class="description">' + jsonObject.description + '</div>';
-// div = div + '</div>';
-// $("#contenu:first").append(div);
-// //on ajoute à la fin de l'élément
-// }
-// $("#contenu:first").append(hr);
-// //on ajoute une ligne hrizontale
 
 function enregistrer_Employe() {
 	var nom = $('input[name="nom"]:first').val();
@@ -152,6 +90,52 @@ function enregistrer_Employe() {
 
 }
 
+// Ajouter un produit
+function enregistrer_Produit() {
+
+	var nomProduit = $('input[name="nomProduit"]:first').val();
+	var descriptionProduit = $('textarea[name="descriptionProduit"]:first').val();
+	var imageProduit = $('#imageProduit').find(":selected").text();
+	var categorieProduit = $('#categorieProduit').find(":selected").text();
+
+	if ((nomProduit == "undefined") || (nomProduit == null) || (descriptionProduit == "undefined") || (descriptionProduit == null) || (imageProduit == "undefined") || (imageProduit == null) || (categorieProduit == "undefined") || (categorieProduit == null)) {
+		alert("Merci de remplir les champs manquants");
+		retourn(false);
+		//on ne change rien si
+	}
+	//methode 1
+	bdProduit = localStorage.getItem('bdProduitjson');
+	if ((bdProduit == "undefined") || (bdProduit == null)) {//ici on doit initialiser la bd
+		var bdProduitjson = {
+			"Produits" : []
+
+		};
+
+		localStorage.setItem('bdProduitjson', JSON.stringify(bdProduitjson));
+		bdProduit = bdProduitjson;
+
+	} else {
+		bdProduit = JSON.parse(bdProduit);
+		//parsing de objet json
+	}
+	var descJsonObjects = bdProduit.Produits;
+	var jsonObject = {//creation de json
+		"nomProduit" : nomProduit,
+		"descriptionProduit" : descriptionProduit,
+		"imageProduit" : imageProduit,
+		"categorieProduit" : categorieProduit
+	};
+	descJsonObjects.push(jsonObject);
+	//ajout dans le tableau des description
+	bdProduit.Produits = descJsonObjects;
+	localStorage.setItem('bdProduitjson', JSON.stringify(bdProduit));
+	
+	//$( "form:first" ).trigger( "reset" );
+	return (true);
+
+}
+
+
 // ******************** Evennements de la page connexion *************************/
 $(document).ready(function() {
 
@@ -164,6 +148,12 @@ $(document).ready(function() {
 	$("#btnEnregistrerEmploye").click(function() {
 
 		return enregistrer_Employe();
+
+	});
+	$("#enregistrerProduit").click(function() {
+
+		return enregistrer_Produit();
+		
 
 	});
 
